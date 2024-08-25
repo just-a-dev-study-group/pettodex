df62d800cf71915fe6d782193b17554bb7ba3215
Jeremy Jobert Peñafiel <jeremyjobert.penafiel@gmail.com>
2024-08-25 16:55:07 +0800

    added simple navbar and routing links

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 const Page = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+
       <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
         <h1 className="text-3xl font-bold mb-6 text-center">Login</h1>
 
