2964dfcc1bf8ca69d88f24142ec18953a33a82e6
Jeremy Jobert Peñafiel <jeremyjobert.penafiel@gmail.com>
2024-08-25 18:06:47 +0800

    used App Router instead of Links for navbar

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,20 +1,30 @@
+"use client"
 import Link from "next/link";
 import {Button} from "@/components/ui/button";
+import {useRouter} from "next/navigation";
 
 
 const NavBar = () => {
+    const router = useRouter();
+
+    const handleBrowsePets = async () => {
+        await router.push("/pets");
+    }
+
+    const handleSubmitPet = async () => {
+        await router.push("/");
+    }
+
     return(
         <nav>
             <div className={`w-screen bg-green-300 p-1 h-24 flex items-center justify-center place-items-center`}>
                 <div className={`w-5/6 grid grid-cols-2 grid-rows-1 items-center`}>
-                        <div className={`flex h-full w-full justify-start items-center`}>
-                        <h1 className="text-3xl font-bold">Pettodex</h1>
+                    <div className={`flex h-full w-full justify-start items-center`}>
+                        <h1 onClick={handleSubmitPet} className="text-3xl font-bold cursor-pointer">Pettodex
+                        </h1>
                     </div>
                     <div className={`flex gap-2 justify-end`}>
-                        <Button>
-                            <Link href="/pets">Browse Pets</Link>
-
-                        </Button>
+                        <Button onClick={handleBrowsePets}>Browse Pets</Button>
                         <Button>
                             <Link href="/">Submit Pet</Link>
                         </Button>
