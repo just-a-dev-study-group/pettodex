a9d2d9ca72eab1248c7729058852f92233b5a000
xpcybe <xpcybe@gmail.com>
2024-08-26 17:33:52 +0800

    Add image upload

--- src/logic/submit-entity.ts
+++ src/logic/submit-entity.ts
@@ -1,28 +1,35 @@
 "use server";
 
 import { db } from "@/lib/firebase/firebase-admin";
+import { validateAuth } from "@/server/utils/validate-auth";
 import { PetEntity } from "@/types/entity.types";
 import { put } from "@vercel/blob";
 
 const submitPetEntity = async (formData: FormData) => {
   try {
+    // Make sure the user is logged in
+    const tokens = await validateAuth();
+
     const name = formData.get("name") as string;
     const imageFile = formData.get("imageFile") as File;
 
     // Generate a new ID for the pet entity
     const newId = db.collection("entity").doc().id;
 
+    tokens.decodedToken.uid;
+
     // Process the form data
     const petData: PetEntity = {
       id: newId,
       name: name,
+      owner_id: tokens.decodedToken.uid,
+      imageUrl: "",
       species_id: "", // This needs to be determined or provided
-      owner_id: "", // This needs to be determined or provided
       sex: "", // This needs to be determined or provided
       color: "", // This needs to be determined or provided
-      imageUrl: "", // This needs to be determined or provided
     };
 
+    // Upload the image file to the Vercel Blob storage
     if (imageFile) {
       const blob = await imageFile.arrayBuffer();
       const originalFileType = imageFile.type.split("/").pop();
@@ -33,8 +40,6 @@ const submitPetEntity = async (formData: FormData) => {
 
       petData.imageUrl = url;
     }
-
-    // TODO: Handle image file upload (imageFile)
 
     await db.collection("entity").doc(newId).set(petData);
     return { success: true, id: newId };
